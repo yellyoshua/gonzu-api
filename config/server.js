243888cfcc182397c7ba1ba2f55e5c41919ecb7f
yell-yoshua <usuario1529yoshua@hotmail.com>
2020-09-19 14:32:51 -0500

    elections model

--- config/server.js
+++ config/server.js
@@ -1,4 +1,15 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-});
+module.exports = ({ env }) => {
+  console.log({ NODE_ENV: env('NODE_ENV') });
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    admin: {
+      auth: {
+        secret: env('ADMIN_JWT_SECRET')
+      }
+    },
+    cron: {
+      enabled: true
+    }
+  };
+};
