2d5192c56fdaeb4be99727a95584c4c7485d4c2f
yell-yoshua <usuario1529yoshua@hotmail.com>
2020-11-15 23:41:24 -0500

    Vote api and stats votes

--- api/votes/controllers/votes.js
+++ api/votes/controllers/votes.js
@@ -47,14 +47,13 @@ module.exports = {
 
     const theElection = _.omit({ ...election }, ['campaigns', 'voters', 'tags']);
     const vote_tags = _.pluck([...votes], 'tag_slug');
-    const vote_tags_count = groupAndCount([...vote_tags]);
+    const vote_voters = _.pluck([...votes], 'voter');
+    const vote_voters_count = vote_voters.length;
+    const vote_tags_count = groupAndCount(vote_voters.map(voter => voter['tag_slug']));
     const vote_tags_uniq = _.uniq([...vote_tags], false);
     const vote_campaigns = _.pluck([...votes], 'campaign_slug');
     const vote_campaigns_count = groupAndCount([...vote_campaigns]);
     const vote_campaigns_uniq = _.uniq([...vote_campaigns], false);
-    const vote_voters = _.pluck([...votes], 'voter_identify');
-    const vote_voters_count = groupAndCount([...vote_voters]);
-    const vote_voters_uniq = _.uniq([...vote_voters], false);
     const vote_count = votes.length;
 
     let tags = [];
@@ -83,7 +82,6 @@ module.exports = {
       vote_campaigns_uniq,
       vote_voters,
       vote_voters_count,
-      vote_voters_uniq,
       vote_count
     };
   },
@@ -118,34 +116,42 @@ module.exports = {
     return null;
   },
   async vote(ctx) {
-    const { campaign_slug = null, voter_identify = null, tag_slug = null } = ctx.request.body;
+    const { campaign_slug = null, first_auth = null, second_auth = null } = ctx.request.body;
     const { election_id } = ctx.params;
 
     const entityElection = await strapi.services.elections.findOne({ id: election_id });
     const election = sanitizeEntity(entityElection, { model: strapi.models.elections });
     const beThereElection = Boolean(election);
 
-    const entityVote = await strapi.services.votes.findOne({ voter_identify });
+    const entityVote = await strapi.services.votes.findOne({ first_auth, second_auth });
     const vote = sanitizeEntity(entityVote, { model: strapi.models.votes });
     const noHasVoted = !Boolean(vote);
 
     if (beThereElection && noHasVoted) {
-      const tags_id = _.pluck(parseDoubleArrToObjArr(election.tags) || [], 'slug');
-      const voters_id = _.pluck(parseDoubleArrToObjArr(election.voters.data) || [], 'ci');
+      const { field: first_auth_field } = election.first_auth;
+      const { field: second_auth_field } = election.second_auth;
+
+      const voters_objs = parseDoubleArrToObjArr(election.voters.data) || [];
+
+      const voter = voters_objs.find(voter => {
+        return voter[first_auth_field] === first_auth && voter[second_auth_field] === second_auth;
+      });
+
       const campaigns_id = _.pluck(parseDoubleArrToObjArr(election.campaigns) || [], 'slug');
 
       function beFounded(arr, expectValue) {
         return arr.indexOf(expectValue) !== -1;
       }
 
-      const canCreate = beFounded(tags_id, tag_slug) && beFounded(voters_id, voter_identify) && beFounded(campaigns_id, campaign_slug);
+      const canCreate = !!voter && beFounded(campaigns_id, campaign_slug);
 
       if (canCreate) {
         const entity = await strapi.services.votes.create({
           election: election_id,
           campaign_slug,
-          voter_identify,
-          tag_slug
+          voter,
+          first_auth,
+          second_auth
         });
         sanitizeEntity(entity, { model: strapi.models.votes });
         return 'ok';
